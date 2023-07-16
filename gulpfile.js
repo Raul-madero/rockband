883214a780d1252516e547aa004b8b525e021448
Raul Madero <r.madero.ramirez@gmail.com>
2023-07-16 15:26:07 -0600

    Comenzamos a dar formato al header

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const {src, dest, watch} = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 function css(done) {
-    src('./src/scss/app.scss') //Identificar el archivo de sass
+    src('./src/scss/**/*.scss') //Identificar el archivo de sass
+        .pipe(plumber())
         .pipe(sass()) //Compilar archivo de sass
         .pipe(dest('./build/css')); //Almacenar en el disco duro
     done();
 };
 function dev(done) {
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
     done()
 }
 exports.css = css;

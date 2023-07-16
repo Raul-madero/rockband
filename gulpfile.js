3ed15ec06b8342f58751ddc9e5448ba7bc69a29d
Raul Madero <r.madero.ramirez@gmail.com>
2023-07-16 14:26:23 -0600

    Cambios

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 function css(done) {
     src('./src/scss/app.scss') //Identificar el archivo de sass
         .pipe(sass()) //Compilar archivo de sass
-        .pipe(dest('./build/css/app.css')); //Almacenar en el disco duro
+        .pipe(dest('./build/css')); //Almacenar en el disco duro
     done();
 };
 exports.css = css;

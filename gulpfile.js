ac73a398e10b45a777a2117c482cfadb585c41c6
Raul Madero <r.madero.ramirez@gmail.com>
2023-07-18 16:37:41 -0600

    Codigo js simplificado

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,15 @@ const avif = require('gulp-avif');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser-js');
 function css(done) {
     src('./src/scss/**/*.scss') //Identificar el archivo de sass
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) //Compilar archivo de sass
         .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/css')); //Almacenar en el disco duro
     done();
 };
@@ -49,6 +53,9 @@ function versionAvif(done) {
 }
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }

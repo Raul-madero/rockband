3f17c4b787f2ae857c3322087aaeb029ca44e3fd
Raul Madero <r.madero.ramirez@gmail.com>
2023-07-18 11:20:07 -0600

    Se creo el overlay de la galeria

--- build/js/app.js
+++ build/js/app.js
@@ -12,7 +12,32 @@ function crearGaleria() {
             <source srcset="build/img/thumb/${i}.avif" type="image/avif">
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img load="lazy" src="img/thumb/${i}.jpg" alt="">    
-        ` 
-        galeria.appendChild(imagen)
-    }
+        ` ;
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        };
+        galeria.appendChild(imagen);
+    };
+};
+function mostrarImagen(id) {
+    const seleccion = document.querySelector('body');
+    const imagen = document.createElement('picture');
+    const cerrarModal = document.createElement('P');
+        imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="image/avif">
+            <source srcset="build/img/grande/${id}.webp" type="image/webp">
+            <img load="lazy" src="img/grande/${id}.jpg" alt="">    
+        ` ;
+        const overlay = document.createElement('DIV');
+        overlay.appendChild(imagen);
+        overlay.classList.add('overlay');
+        cerrarModal.textContent = 'X';
+        cerrarModal.classList.add('btn-cerrar');
+        cerrarModal.onclick = function() {
+            seleccion.classList.remove('fijar-body')
+            overlay.remove();
+        };
+        overlay.appendChild(cerrarModal);
+        seleccion.appendChild(overlay);
+        seleccion.classList.add('fijar-body');
 }
